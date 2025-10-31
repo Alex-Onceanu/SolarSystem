1dd0ebdfe5414cc4dd8ff17d3525c8f75f59046d
Alex Onceanu <4lex.onceanu@gmail.com>
2025-10-31 20:36:06 +0100

    Removed earth png

--- src/init.c
+++ src/init.c
@@ -228,7 +228,7 @@ unsigned int init(GLFWwindow** window)
 
     glfwMakeContextCurrent(*window);
     glfwSetWindowSizeLimits(*window, LOW_RES_W, LOW_RES_H, GLFW_DONT_CARE, GLFW_DONT_CARE);
-    // glfwSwapInterval(0);
+    glfwSwapInterval(0);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
